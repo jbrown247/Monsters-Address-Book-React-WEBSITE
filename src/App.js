51b9f1dcfcc83806ee664e5e666879a522644ff1
Jeff Brown <ffsale24@gmail.com>
2019-09-11 23:48:31 -0700

    API Fetch Monster List Array of Users from JSON

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'as1w'
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
@@ -36,3 +29,4 @@ class App extends Component {
 };
 
 export default App;
+
