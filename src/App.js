c87ad67706c37487c91a01d1dfd9f9993e56444d
Jeff Brown <ffsale24@gmail.com>
2019-09-23 19:48:12 -0700

    Google Fonts Import Title Styling H1

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monster Address Book </h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
