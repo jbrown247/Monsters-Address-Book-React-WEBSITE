b51602537ab1ccf4de0710157151017de093e847
Jeff Brown <ffsale24@gmail.com>
2019-09-27 00:59:42 -0700

    Updates

---
+++ precache-manifest.8a1c548bf38d007c73309b3b763d5ade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eec1af35688c9721c9a30f1d9d971bf",
+    "url": "/Monsters-Address-Book-React-WEBSITE/index.html"
+  },
+  {
+    "revision": "a83c6e2685c7dc3e9624",
+    "url": "/Monsters-Address-Book-React-WEBSITE/static/css/main.ad95f197.chunk.css"
+  },
+  {
+    "revision": "18d6487f772f4b48d8c6",
+    "url": "/Monsters-Address-Book-React-WEBSITE/static/js/2.3352609d.chunk.js"
+  },
+  {
+    "revision": "a83c6e2685c7dc3e9624",
+    "url": "/Monsters-Address-Book-React-WEBSITE/static/js/main.e48a772a.chunk.js"
+  },
+  {
+    "revision": "60a6135f154cfe37f487",
+    "url": "/Monsters-Address-Book-React-WEBSITE/static/js/runtime~main.c926e920.js"
+  }
+]);
